014ae9bc8702a000ea99665020f11db740f10479
unknown <vanndyu@gmail.com>
2020-09-20 15:59:52 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <scanner></scanner>
+  </div>
+</template>
+
+<script>
+import scanner from './components/Scanner'
+
+export default {
+  name: 'App',
+  components: {
+    scanner
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+    *
+{
+    box-sizing: border-box;
+}
+body{
+    margin: 0;
+    overflow-x: hidden;
+}
+</style>
